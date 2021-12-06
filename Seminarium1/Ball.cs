032754762b166d9d3b74700e67e50633ae6d2c4b
eric.kindberg01@gmail.com <eric.kindberg01@gmail.com>
2021-12-06 18:28:29 +0100

    Add project files.

---
+++ Seminarium1/Ball.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seminarium1
+{
+    public class Ball
+    {
+        private Vector2 position, direction;
+        private Texture2D texture;
+        private double elapsed;
+        private int _width, _height;
+        public Vector2 Direction { get => direction; set=> direction=value; }
+        public int Size { private get; set; }
+        public float Speed { private get; set; }
+        public Ball(Texture2D tex, Vector2 pos, Vector2 direction, int height, int width, int size)
+        {
+            elapsed = 0;
+            texture = tex;
+            position = pos;
+            this.direction = direction;
+            _height = height;
+            _width = width;
+            Speed = 10;
+            Size = size;
+        }
+        public void Update(GameTime gameTime)
+        {
+            position += direction * Speed;
+            if (position.X >= _width-texture.Width && direction.X>0)
+            {
+                direction.X = -direction.X;
+            }
+            else if(position.X<=0 && direction.X < 0)
+            {
+                direction.X = -direction.X;
+            }
+            else if(position.Y>=_height-texture.Height && direction.Y > 0)
+            {
+                direction.Y = -direction.Y;
+            }
+            else if(position.Y<=0 && direction.Y < 0)
+            {
+                direction.Y = -direction.Y;
+            }
+
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, new Rectangle((int)position.X, (int)position.Y, Size, Size), Color.White);
+        }
+        public void SetPosition(Vector2 value)
+        {
+            position = value;
+        }
+        public Rectangle HitBox()
+        {
+            return new Rectangle((int)position.X,(int)position.Y,Size,Size);
+        }
+    }
+}
---
+++ Seminarium1/Game1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Seminarium1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D ballTex;
+        private Ball ball1;
+        private Ball ball2;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+        }
+
+        protected override void Initialize()
+        {
+            
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            ballTex = Content.Load<Texture2D>("ball");
+            
+            ball1 = new Ball(ballTex,new Vector2(800,100),new Vector2(1,2), Window.ClientBounds.Height,Window.ClientBounds.Width,50);
+            ball2 = new Ball(ballTex, new Vector2(000,100),new Vector2(-1,-1),Window.ClientBounds.Height, Window.ClientBounds.Width,50);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            ball1.Update(gameTime);
+            ball2.Update(gameTime);
+
+            if (ball1.HitBox().Intersects(ball2.HitBox()))
+            {
+                ball1.Direction = -ball1.Direction;
+                ball2.Direction = -ball2.Direction;
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            _spriteBatch.Begin();
+            ball1.Draw(_spriteBatch);
+            ball2.Draw(_spriteBatch);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
